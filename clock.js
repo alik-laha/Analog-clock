08ce0094a6f442fe53289bf4fb25e582e1de16fa
Alik Laha <aliklaha0@gmail.com>
2023-06-07 21:57:34 +0530

    adding digital clock

--- clock.js
+++ clock.js
@@ -1,12 +1,12 @@
+let mm = document.getElementById('mini')
+let ss = document.getElementById('sec')
+let hh = document.getElementById('hour')
 setInterval(()=>{
     let d = new Date();
-    let mm = d.getMinutes();
-    let ss = d.getSeconds();
-    let hh = d.getHours();
-    let mmdeg = mm*6-90;
-    let ssdeg = ss*6-90;
-    let hhdeg = 30*hh+mm/12-90;
-    mini.style.transform = ` rotate (${mmdeg}deg)`;
+    let mmdeg = d.getMinutes()*6-90;
+    let ssdeg = d.getSeconds()*6-90;
+    let hhdeg = (30* d.getHours())+(mmdeg/12)-90;
+    mini.style.transform = `rotate(${mmdeg}deg)`;
     sec.style.transform = `rotate(${ssdeg}deg)`;
     hour.style.transform = `rotate(${hhdeg}deg)`;
 },1000);
