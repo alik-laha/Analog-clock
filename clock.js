05ef97e76d8ecb2559c40f700878f742508a6e24
Alik Laha <aliklaha0@gmail.com>
2023-06-07 17:08:04 +0530

    runing hands

--- clock.js
+++ clock.js
@@ -1,3 +1,12 @@
-let mm = document.getElementsByClassName('mini')
-let hh = document.getElementsByClassName('hour')
-let ss = document.getElementsByClassName('sec')
+setInterval(()=>{
+    let d = new Date();
+    let mm = d.getMinutes();
+    let ss = d.getSeconds();
+    let hh = d.getHours();
+    let mmdeg = mm*6-90;
+    let ssdeg = ss*6-90;
+    let hhdeg = 30*hh+mm/12-90;
+    mini.style.transform = ` rotate (${mmdeg}deg)`;
+    sec.style.transform = `rotate(${ssdeg}deg)`;
+    hour.style.transform = `rotate(${hhdeg}deg)`;
+},1000);
